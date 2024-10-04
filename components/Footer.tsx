7c5caf7f6afa6386f34952fff807bf89258c986e
tadeo <94108039+tadeongmi@users.noreply.github.com>
2024-10-04 21:03:08 +0100

    modify footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Footer() {
   return (
     <footer className="bg-black text-white py-2 px-4 text-center">
-      <p>&copy; 2024 EgUSDC PoC by Juan Samitier. All rights reserved.</p>
+      <p>&copy; 2024 Eager Protocol by Juan Samitier & Bri. All rights reserved.</p>
     </footer>
   );
 }
